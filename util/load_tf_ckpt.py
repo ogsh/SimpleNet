b7e83a44b8f40a3c6ff7e69d1dfd4e0cf88a9ec2
ogsh <nct.ogsh@gmail.com>
2018-07-28 21:45:10 +0900

    first commit

--- util/load_tf_ckpt.py
+++ util/load_tf_ckpt.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from tensorflow.python.tools import inspect_checkpoint as ckpt
-from tensorflow.python import pywrap_tensorflow
+import numpy as np
 
+from collections import OrderedDict
 #def load_tf_ckpt(model):
-
 
 
 model_path = "../tensorflow-yolo-v3/export/model.ckpt"
@@ -11,10 +11,15 @@ model_path = "../tensorflow-yolo-v3/export/model.ckpt"
 #ckpt.print_tensors_in_checkpoint_file(model_path, tensor_name='', all_tensors=True)
 
 all_tensors = all_tensor_names = True
-reader = pywrap_tensorflow.NewCheckpointReader(model_path)
+reader = tf.python.pywrap_tensorflow.NewCheckpointReader(model_path)
+
+tf_dict = OrderedDict()
+
 if all_tensors or all_tensor_names:
   var_to_shape_map = reader.get_variable_to_shape_map()
   for key in sorted(var_to_shape_map):
     print("tensor_name: ", key)
     print(reader.get_tensor(key).shape)
+    tf_dict[key] = np.array(reader.get_tensor(key))
+
 
